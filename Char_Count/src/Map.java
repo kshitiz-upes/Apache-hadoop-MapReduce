f72aac475a4ba79c77562213a8941bd4894262fe
kshitiz gupta <kshitizgupta82@gmail.com>
2019-10-29 22:36:24 +0530

    Update Map.java

--- Char_Count/src/Map.java
+++ Char_Count/src/Map.java
@@ -6,9 +6,11 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;    
 import org.apache.hadoop.mapred.OutputCollector;    
 import org.apache.hadoop.mapred.Reporter;    
-public class Map extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{    
+public class Map extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>
+{    
     public void map(LongWritable key, Text value,OutputCollector<Text,IntWritable> output,     
-           Reporter reporter) throws IOException{    
+           Reporter reporter) throws IOException
+    {    
         String line = value.toString();    
         String  tokenizer[] = line.split("");    
         for(String SingleChar : tokenizer)  
