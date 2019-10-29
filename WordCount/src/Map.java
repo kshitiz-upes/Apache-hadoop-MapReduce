4e190045eaa53fa96733938c26ee10ccdf3f9dd5
kshitiz gupta <kshitizgupta82@gmail.com>
2019-10-29 22:35:38 +0530

    Update Map.java

--- WordCount/src/Map.java
+++ WordCount/src/Map.java
@@ -6,17 +6,17 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class Map
-       extends Mapper<Object, Text, Text, IntWritable>{
-
+       extends Mapper<Object, Text, Text, IntWritable>
+ {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
-
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+    public void map(Object key, Text value, Context context) 
+           throws IOException, InterruptedException
+    {
       StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
+           {
+               word.set(itr.nextToken());
+               context.write(word, one);
+           }
     }
-  }
+}
